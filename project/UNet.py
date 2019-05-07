fixed CrossEntropyLoss computation with class labeling

project/UNet.py
@@ -158,6 +158,9 @@ def train_UNet(device, unet, dataset, width_out, height_out, epochs=1):
         patch_counter = 0
 
         for batch_ndx, sample in enumerate(patch_loader):
+            if patch_counter >= 1:
+                break
+
             for i in range(batch_size):
                 # Forward part
                 patch_name = sample['patch_name'][i]
@@ -177,7 +180,9 @@ def train_UNet(device, unet, dataset, width_out, height_out, epochs=1):
                 output = output.resize(m * width_out * height_out, 5)  # was 2, allows the resize to maintain 5 channels, I believe.
                 label = label.resize(m * width_out * height_out, 5)  # was nothing
 
-                loss = criterion(output, torch.max(label, 1)[1])  # CrossEntropyLoss does not expect a one-hot encoded vector as the target, but class indices
+                # loss = criterion(output, torch.max(label, 1)[1])  # CrossEntropyLoss does not expect a one-hot encoded vector as the target, but class indices
+                loss = criterion(output, torch.argmax(label, 1))
+
                 loss.backward()
                 optimizer.step()
 

project/tools/inspect_model.py
@@ -1,10 +1,16 @@
 from torch.nn.modules.module import _addindent
 from UNet import UNet
+from parameters import *
 import torch
 import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+from PatchDataset import PatchDataset
+from torch.utils.data import DataLoader
 
 
-path = 'D:/Bachelor_Project/VU_Bachelor_Project/project/models/test1.pickle'
+paths = get_paths()
+path = '{}/test1.pickle'.format(paths['model_dir'])
 
 
 def torch_summarize(model, show_weights=True, show_parameters=True):
@@ -35,8 +41,65 @@ def torch_summarize(model, show_weights=True, show_parameters=True):
     return tmpstr
 
 
+def plot_5L_tensors(raw, output):
+    raw = raw.numpy()
+    output = output.detach().numpy()  # detaches grad from variable
+
+    plt.subplot(2, 3, 1)
+    plt.imshow(raw)
+
+    plt.subplot(2, 3, 2)
+    plt.imshow(output[0, 0, :, :])
+
+    plt.subplot(2, 3, 3)
+    plt.imshow(output[0, 1, :, :])
+
+    plt.subplot(2, 3, 4)
+    plt.imshow(output[0, 2, :, :])
+
+    plt.subplot(2, 3, 5)
+    plt.imshow(output[0, 3, :, :])
+
+    plt.subplot(2, 3, 6)
+    plt.imshow(output[0, 4, :, :])
+
+    plt.show()
+
+
+def plot_2d_tensors(raw, output):
+    raw = raw.numpy()
+    output = output.detach().numpy()  # detaches grad from variable
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(raw)
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(output[0, :, :])
+
+    plt.show()
+
+
 model = UNet(in_channel=1, out_channel=5)
 model.load_state_dict(torch.load(path))
-model.eval()
+# model.eval()
 
 print(torch_summarize(model))
+
+patches = PatchDataset(paths['out_dir'], torch.device('cpu'))
+
+# output = model(patches[0]['raw'][None][None])
+#
+# print(output.shape)
+# plot_5L_tensors(patches[0]['raw'], output)
+#
+# predicted, _ = torch.max(output, 1)
+# print(predicted.shape)
+# plot_2d_tensors(patches[0]['raw'], predicted)
+
+with torch.no_grad():
+    for i in range(5):
+        output = model(patches[i]['raw'][None][None])
+        plot_5L_tensors(patches[i]['raw'], output)
+
+        # predicted, _ = torch.max(output, 1)
+        # plot_2d_tensors(patches[i]['raw'], predicted)